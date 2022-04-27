9dc6291e380a5afb7f885d2135f283813926786c
Christopher Koski <82855685+ChrisKoskiii@users.noreply.github.com>
2022-04-27 13:18:49 -0400

    refactor: custom view modifier for strokeStyle

--- DesignCodeiOS15/DesignCodeiOS15/ContentView.swift
+++ DesignCodeiOS15/DesignCodeiOS15/ContentView.swift
@@ -15,28 +15,57 @@ struct ContentView: View {
         .resizable(resizingMode: .stretch)
         .aspectRatio(contentMode: .fit)
         .frame(width: 26.0, height: 26.0)
-        .cornerRadius(20.0)
+        .cornerRadius(10.0)
+        .padding(9)
+        .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 16, style: .continuous))
+        .strokeStyle(cornerRadius: 16)
+      
       Text("SwiftUI iOS 15")
         .font(.largeTitle)
         .fontWeight(.bold)
-      Text("20 sections - 3 hours")
+        .foregroundStyle(.linearGradient(colors: [.primary, .primary.opacity(0.5)], startPoint: .topLeading, endPoint: .bottomTrailing))
+      Text("20 sections - 3 hours".uppercased())
         .font(.footnote)
         .fontWeight(.semibold)
+        .foregroundStyle(.secondary)
       Text("Design and code a SwiftUI 3 app with custom layouts, animations and gestures using Xcode 13, SF Symbols 3, Canvas, Concurrency, Searchable and a whole lot more")
         .font(.footnote)
-          .multilineTextAlignment(.leading)
-          .lineLimit(2)
+        .multilineTextAlignment(.leading)
+        .lineLimit(2)
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .foregroundColor(.secondary)
     }
     .padding(.all, 20.0)
+    .padding(.vertical, 20)
     .frame(height: 350.0)
-    .background(Color("Background"))
-    .cornerRadius(30.0)
-    .shadow(radius: 20)
+    .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 30, style: .continuous))
+//    .cornerRadius(30.0)
+//    .mask(RoundedRectangle(cornerRadius: 30.0, style: .continuous))
+    .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
+    .strokeStyle()
+    .padding(.horizontal)
+    .background(
+      Image("Blob 1")
+        .offset(x: 250, y: -100)
+    )
+    .overlay(
+      Image("Illustration 5")
+        .resizable()
+        .aspectRatio(contentMode: .fit)
+        .frame(height: 230)
+        .offset(x: 32, y: -80)
+    )
+
   }
 }
 
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
-    ContentView()
+    Group {
+      ContentView()
+      ContentView()
+        .preferredColorScheme(.dark)
+    }
+    
   }
 }
