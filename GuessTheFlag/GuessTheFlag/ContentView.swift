b9d7f62681b3ab2a31bc109619eeb98091307a12
Christopher Koski <82855685+ChrisKoskiii@users.noreply.github.com>
2022-04-26 20:06:18 -0400

    start GuessTheFlag

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,14 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+  var correctAnswer = Int.random(in: 0...2)
+  
+  var body: some View {
+    ZStack {
+      Color.blue
+        .ignoresSafeArea()
+      VStack {
+        VStack(spacing: 30) {
+          Text("Tap the flag of")
+            .foregroundColor(.white)
+          Text(countries[correctAnswer])
+            .foregroundColor(.white)
+        }
+        ForEach(0..<3) { number in
+          Button {
+            //action
+          } label: {
+            Image(countries[number])
+              .renderingMode(.original)
+          }
+        }
+      }
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
