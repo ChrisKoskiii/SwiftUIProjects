1619d0f65e41294564e74863aac5a13933fb0731
Christopher Koski <82855685+ChrisKoskiii@users.noreply.github.com>
2022-05-04 15:42:01 -0400

    custom toolbar

---
+++ DesignCodeiOS15/DesignCodeiOS15/Components/FeaturedItem.swift
@@ -0,0 +1,20 @@
+//
+//  FeaturedItem.swift
+//  DesignCodeiOS15
+//
+//  Created by Christopher Koski on 5/4/22.
+//
+
+import SwiftUI
+
+struct FeaturedItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FeaturedItem_Previews: PreviewProvider {
+    static var previews: some View {
+        FeaturedItem()
+    }
+}
---
+++ DesignCodeiOS15/DesignCodeiOS15/Model/PreferenceKeys.swift
@@ -0,0 +1,20 @@
+//
+//  PreferenceKeys.swift
+//  DesignCodeiOS15
+//
+//  Created by Christopher Koski on 5/4/22.
+//
+
+import SwiftUI
+
+struct PreferenceKeys: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PreferenceKeys_Previews: PreviewProvider {
+    static var previews: some View {
+        PreferenceKeys()
+    }
+}
