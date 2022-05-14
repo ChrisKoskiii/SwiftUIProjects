8c2e91f7eb03baf9658ffdb7c514272e139be558
Christopher Koski <82855685+ChrisKoskiii@users.noreply.github.com>
2022-05-14 10:10:21 -0400

    minor refactor

--- ThirstyHQ/ThirstyHQ/Views/ContentView.swift
+++ ThirstyHQ/ThirstyHQ/Views/ContentView.swift
@@ -17,46 +17,9 @@ struct ContentView: View {
     
     NavigationView {
       ZStack {
-        
         VStack {
-          let columns: [GridItem] = Array(repeating: .init(.adaptive(minimum: 120)), count: 2)
-          
           ScrollView {
-            LazyVGrid(columns: columns) {
-              ForEach (cocktailAPI.drinks, id: \.self) { drink in
-                VStack(alignment: .leading, spacing: 8) {
-                  NavigationLink {
-                    DetailView(drink: drink)
-                  } label : {
-                    VStack {
-                      AsyncImage(url: URL(string: drink.strDrinkThumb!)) { phase in
-                        if let image = phase.image {
-                          image
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 100, height: 100)
-                            .cornerRadius(20)
-                            .padding(.top)
-                            .padding(.leading)
-                            .padding(.trailing)
-                        } else if phase.error != nil {
-                          Color.red
-                            .frame(width: 100, height: 100)
-                        } else {
-                          Color.blue
-                        }
-                      }
-                      Text(drink.strDrink ?? "Cocktail")
-                        .font(.title3.bold())
-                        .foregroundStyle(.linearGradient(colors: [.primary, .primary.opacity(0.7)], startPoint: .leading, endPoint: .trailing))
-                    }
-                    .background(.ultraThinMaterial, in:
-                                  RoundedRectangle(cornerRadius: 20 )
-                    )
-                  }
-                }
-              }
-            }
+            DrinkGridView(cocktailAPI: cocktailAPI)
           }
         }
         .navigationTitle("Popular")
@@ -71,6 +34,49 @@ struct ContentView: View {
     }
   }
 }
+
+struct DrinkGridView: View {
+  @ObservedObject var cocktailAPI: CocktailDBAPI
+  var body: some View {
+    let columns: [GridItem] = Array(repeating: .init(.adaptive(minimum: 120)), count: 2)
+    LazyVGrid(columns: columns) {
+      ForEach (cocktailAPI.drinks, id: \.self) { drink in
+        VStack(alignment: .leading, spacing: 8) {
+          NavigationLink {
+            DetailView(drink: drink)
+          } label : {
+            VStack {
+              AsyncImage(url: URL(string: drink.strDrinkThumb!)) { phase in
+                if let image = phase.image {
+                  image
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100, height: 100)
+                    .cornerRadius(20)
+                    .padding(.top)
+                    .padding(.leading)
+                    .padding(.trailing)
+                } else if phase.error != nil {
+                  Color.red
+                    .frame(width: 100, height: 100)
+                } else {
+                  Color.blue
+                }
+              }
+              Text(drink.strDrink ?? "Cocktail")
+                .font(.title3.bold())
+                .foregroundStyle(.linearGradient(colors: [.primary, .primary.opacity(0.7)], startPoint: .leading, endPoint: .trailing))
+            }
+            .background(.ultraThinMaterial, in:
+                          RoundedRectangle(cornerRadius: 20 )
+            )
+          }
+        }
+      }
+    }
+  }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
