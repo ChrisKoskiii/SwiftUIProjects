8291eafc269fe926059652be056b97e6bdaf96f0
Christopher Koski <82855685+ChrisKoskiii@users.noreply.github.com>
2022-05-16 16:32:14 -0400

    Memorize initial commit

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Christopher Koski on 5/16/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
